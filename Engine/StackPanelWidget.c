378b28d90061ce1d3d5b6d410b8f0f83512f6c59
Jim Marshall <jim.marshall@espritautomation.com>
2025-04-11 14:53:39 +0100

    GetWidth + GetHeight implemented for stack panel widget

--- Engine/StackPanelWidget.c
+++ Engine/StackPanelWidget.c
@@ -16,14 +16,114 @@ struct StackPanelWidgetData
 	struct WidgetPadding padding;
 };
 
-static void GetWidth(struct UIWidget* pWidget, struct UIWidget* pParent)
+static float GetWidth(struct UIWidget* pWidget, struct UIWidget* pParent)
 {
+	struct StackPanelWidgetData* pStackPanelData = pWidget->pImplementationData;
+	float width = 0.0;
 
+	if (pWidget->hFirstChild == NULL_HWIDGET)
+	{
+		return width;
+	}
+	switch (pStackPanelData->orientation)
+	{
+	case SPO_Horizontal:
+		{
+			struct UIWidget* pChild = UI_GetWidget(pWidget->hFirstChild);
+			while (pChild)
+			{
+				width += pChild->fnGetWidth(pChild, pWidget);
+				if (pChild->hNext == NULL_HWIDGET)
+				{
+					pChild = NULL;
+				}
+				else
+				{
+					pChild = UI_GetWidget(pChild->hNext);
+				}
+			}
+			break;
+		}
+	case SPO_Vertical:
+		{
+			float widest = 0.0;
+			struct UIWidget* pChild = UI_GetWidget(pWidget->hFirstChild);
+			while (pChild)
+			{
+				float thisChildW = pChild->fnGetWidth(pChild, pWidget);
+				if (thisChildW > widest)
+				{
+					widest = thisChildW;
+				}
+				if (pChild->hNext == NULL_HWIDGET)
+				{
+					pChild = NULL;
+				}
+				else
+				{
+					pChild = UI_GetWidget(pChild->hNext);
+				}
+			}
+			width = widest;
+			break;
+		}
+	}
+	return width;
 }
 
-static void GetHeight(struct UIWidget* pWidget, struct UIWidget* pParent)
+static float GetHeight(struct UIWidget* pWidget, struct UIWidget* pParent)
 {
+	struct StackPanelWidgetData* pStackPanelData = pWidget->pImplementationData;
+	float height = 0.0;
 
+	if (pWidget->hFirstChild == NULL_HWIDGET)
+	{
+		return height;
+	}
+	switch (pStackPanelData->orientation)
+	{
+	case SPO_Horizontal:
+	{
+		float tallest = 0.0;
+		struct UIWidget* pChild = UI_GetWidget(pWidget->hFirstChild);
+		while (pChild)
+		{
+			float thisChildH = pChild->fnGetHeight(pChild, pWidget);
+			if (thisChildH > tallest)
+			{
+				tallest = thisChildH;
+			}
+			if (pChild->hNext == NULL_HWIDGET)
+			{
+				pChild = NULL;
+			}
+			else
+			{
+				pChild = UI_GetWidget(pChild->hNext);
+			}
+		}
+		height = tallest;
+		break;
+	}
+	case SPO_Vertical:
+	{
+		struct UIWidget* pChild = UI_GetWidget(pWidget->hFirstChild);
+		while (pChild)
+		{
+			height += pChild->fnGetHeight(pChild, pWidget);
+			if (pChild->hNext == NULL_HWIDGET)
+			{
+				pChild = NULL;
+			}
+			else
+			{
+				pChild = UI_GetWidget(pChild->hNext);
+			}
+		}
+		break;
+	}
+	}
+	return height;
 }
 
 static void LayoutChildren(struct UIWidget* pWidget, struct UIWidget* pParent)
