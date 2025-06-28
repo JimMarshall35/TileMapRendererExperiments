a5b1556b59d9dfcae3233274d6223d0502cdb980
Jim Marshall <jimmarshall35@gmail.com>
2025-06-28 13:47:40 +0100

    lua dependency changed to lua5.4

--- Stardew/GetDependenciesUbuntu.sh
+++ Stardew/GetDependenciesUbuntu.sh
@@ -1,1 +1,2 @@
-sudo apt-get install -y libxml2 liblua5.1-0-dev libxml2-dev libbox2d-dev libglfw3-dev libfreetype-dev libgtest-dev
+sudo apt-get install -y libxml2 liblua5.4-dev libxml2-dev libbox2d-dev libglfw3-dev libfreetype-dev libgtest-dev
+cd ..
