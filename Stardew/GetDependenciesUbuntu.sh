fdeabc39e851e09ad1d0ce23eacabac19d892c17
Jim Marshall <46267679+JimMarshall35@users.noreply.github.com>
2025-06-28 12:03:33 +0100

    Update GetDependenciesUbuntu.sh

--- Stardew/GetDependenciesUbuntu.sh
+++ Stardew/GetDependenciesUbuntu.sh
@@ -1,1 +1,1 @@
-sudo apt-get install -y libxml2 liblua5.1-0-dev libxml2-dev libbox2d-dev libglfw3-dev libfreetype-dev
+sudo apt-get install -y libxml2 liblua5.1-0-dev libxml2-dev libbox2d-dev libglfw3-dev libfreetype-dev libgtest-dev
