07e03a1b53b3c4a6ddbcb31a2b9a6f2b6727cbaf
Jim Marshall <jim.marshall@codethink.co.uk>
2025-10-03 22:45:28 +0100

    compile asset script temporary workaround for linux

--- Stardew/compile_assets.sh
+++ Stardew/compile_assets.sh
@@ -4,3 +4,7 @@ python3 engine/scripts/ConvertTiled.py ./Assets/out -m ./Assets/Farm.json ./Asse
 
 
 ./build/atlastool/AtlasTool ./Assets/ui_atlas.xml -o ./Assets/ui_atlas.atlas -bmp UIAtlas.bmp -iw 128 -ih 128
+
+# really the python tool should do this but it doesn't work - cba to fix it right now - this is just as good - the same thing
+./build/atlastool/AtlasTool ./Assets/out/atlas.xml -o ./Assets/out/main.atlas -bmp UIAtlas.bmp -iw 128 -ih 128
+./build/atlastool/AtlasTool ./Assets/out/atlas.xml -o ./Assets/out_rle/main.atlas -bmp UIAtlas.bmp -iw 128 -ih 128
